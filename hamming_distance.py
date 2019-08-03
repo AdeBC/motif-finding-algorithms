9774bc8e401ad3898ae8a202922902fc1c2debc9
Chonghui-007 <ch37915405887@gmail.com>
2019-08-03 17:45:10 +0800

    deleted:    cal_entropy.py 	deleted:    dataset_160_9.txt 	deleted:    dataset_161_5.txt 	deleted:    gibbs_sampler.py 	deleted:    greedy_motif_search.py 	deleted:    hamming_distance.py 	deleted:    median_string.py 	deleted:    motif_enumeration.py 	deleted:    randomized_motif_search.py

--- hamming_distance.py
+++
@@ -1,8 +0,0 @@
-def hammingd(seq1, seq2):
-	c=0
-	for i in range(len(seq1)):
-		if seq1[i]!=seq2[i]: c+=1
-	print(c)
-
-
-hammingd('CTACAGCAATACGATCATATGCGGATCCGCAGTGGCCGGTAGACACACGT','CTACCCCGCTGCTCAATGACCGGGACTAAAGAGGCGAAGATTATGGTGTG')
